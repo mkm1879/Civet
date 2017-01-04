5a8197c62a46c6a70e642d06f833a50c5bbb825a
mkm1879 <mkm1879@gmail.com>
2017-01-04 09:07:11 -0500

    Fixed in-box date sorting bug (wrong branch).  Fixed labels on Participant.

--- src/edu/clemson/lph/civet/files/DateCellComparator.java
+++ src/edu/clemson/lph/civet/files/DateCellComparator.java
@@ -1,16 +1,21 @@
 package edu.clemson.lph.civet.files;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Comparator;
-import java.util.Date;
+import org.apache.log4j.Logger;
+import edu.clemson.lph.civet.Civet;
 
-public class DateCellComparator implements Comparator<java.util.Date> {
+public class DateCellComparator implements Comparator<String> {
+	private static final Logger logger = Logger.getLogger(Civet.class.getName());
+	private SimpleDateFormat df = new SimpleDateFormat( "MMM d, yyyy");
 
 	public DateCellComparator() {
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
-	public int compare(Date arg0, Date arg1) {
+	public int compare(String arg0, String arg1) {
 		if( arg0 == null && arg1 == null)
 			return 0;
 		if( arg0 == null && arg1 != null)
@@ -18,11 +23,20 @@ public class DateCellComparator implements Comparator<java.util.Date> {
 		if( arg0 != null && arg1 == null)
 			return -1;
 		if( arg0 != null && arg1 != null ) {
-			if( arg0.getTime() == arg1.getTime() )
+			java.util.Date date0 = null;
+			java.util.Date date1 = null;
+			try {
+				date0 = df.parse(arg0);
+				date1 = df.parse(arg1);
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				logger.error(e);
+			}
+			if( date0.getTime() == date1.getTime() )
 				return 0;
-			if( arg0.getTime() > arg1.getTime() )
+			if( date0.getTime() > date1.getTime() )
 				return 1;
-			if( arg0.getTime() < arg1.getTime() )
+			if( date0.getTime() < date1.getTime() )
 				return -1;
 		}
 		return 0;
