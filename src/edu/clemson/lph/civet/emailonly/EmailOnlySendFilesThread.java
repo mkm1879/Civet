9dc03df0eb2148cba3dfdd252d4384be1527b8bf
Michael Martin <mkm1879@gmail.com>
2018-02-08 07:30:24 -0500

    Handle bad files in Email Only Out folder

--- src/edu/clemson/lph/civet/emailonly/EmailOnlySendFilesThread.java
+++ src/edu/clemson/lph/civet/emailonly/EmailOnlySendFilesThread.java
@@ -68,8 +68,10 @@ public class EmailOnlySendFilesThread extends Thread {
 				if( fNext != null && fNext.exists() && fNext.isFile() ) {
 					String sName = fNext.getName();
 					String sParts[] = sName.split("\\_");
-					if( sParts.length < 4 )
-						throw new Exception(sName + " is not configured as email only file.");
+					if( sParts.length < 4 ) {
+						MessageDialog.messageWait(parent, "Civet: Email only error", sName + " is not configured as email only file.");
+						continue;
+					}
 					String sToState = sParts[2];
 					ArrayList<File> aStatePdfs = mStateMap.get(sToState);
 					if( aStatePdfs == null ) {
