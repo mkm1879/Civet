a904a16103c89efde2e7deb244dfd2b324dfcc3d
mkm1879@gmail.com <mkm1879@gmail.com>
2018-09-07 16:57:21 -0400

    Added V2 conversion to robot mode

--- src/edu/clemson/lph/civet/robot/COKSRobot.java
+++ src/edu/clemson/lph/civet/robot/COKSRobot.java
@@ -18,6 +18,7 @@ You should have received a copy of the Lesser GNU General Public License
 along with Civet.  If not, see <http://www.gnu.org/licenses/>.
 */
 import edu.clemson.lph.civet.Civet;
+import edu.clemson.lph.civet.CivetInbox;
 import edu.clemson.lph.civet.prefs.CivetConfig;
 import edu.clemson.lph.civet.webservice.CivetWebServiceFactory;
 import edu.clemson.lph.civet.webservice.CivetWebServices;
@@ -25,6 +26,7 @@ import edu.clemson.lph.civet.webservice.CivetWebServicesNew;
 import edu.clemson.lph.civet.xml.CoKsXML;
 import edu.clemson.lph.civet.xml.CviMetaDataXml;
 import edu.clemson.lph.civet.xml.StdeCviXml;
+import edu.clemson.lph.civet.xml.V2Transform;
 import edu.clemson.lph.pdfgen.PDFUtils;
 import edu.clemson.lph.utils.FileUtils;
 
@@ -36,6 +38,7 @@ import java.io.UnsupportedEncodingException;
 import java.rmi.RemoteException;
 
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.w3c.dom.Node;
 
 
@@ -74,6 +77,29 @@ public class COKSRobot extends Thread {
 		bCancel = true;
 	}
 	
+	private boolean isMCvi( File f ) {
+		boolean bRet = false;
+		String sDataFile = FileUtils.getRoot(f.getAbsolutePath()) + ".xml";
+		System.out.println(sDataFile);
+		File fData = new File(sDataFile);
+		if( fData.exists() && fData.isFile() )
+			bRet = true;
+		return bRet;
+	}
+	
+	public static void main( String args[] ) {
+		PropertyConfigurator.configure("CivetConfig.txt");
+		CivetConfig.checkAllConfig();
+		try {
+			COKSRobot me = new COKSRobot();
+			File f = new File("../../Documents/CivetTest/CivetInBox/99-1117582044-1536259117.pdf");
+			System.out.println(me.isMCvi(f));
+		} catch (IOException e) {
+			e.printStackTrace();;
+		}
+		
+	}
+	
 	private void processFiles() {
 		File[] files = inDir.listFiles( new FileFilter() {
 			@Override
@@ -86,65 +112,96 @@ public class COKSRobot extends Thread {
 		});
 		java.util.Date now = new java.util.Date();
 		for( File file : files ) {
-			File fXML = new File( xmlDir, file.getName() + ".cvi" );
+			File fXMLOut = new File( xmlDir, file.getName() + ".cvi" );
+			File fDataFile = null;
 			try {
 				byte[] pdfBytes = FileUtils.readBinaryFile( file );
-				if( PDFUtils.isXFA(pdfBytes) ) {
-					Node xmlNode = PDFUtils.getXFADataNode(pdfBytes);
-					CoKsXML coks = new CoKsXML( xmlNode );
-					String sXML = null;
-					if( bStd ) {
-						StdeCviXml stdXml = coks.getStdeCviXml();
+				boolean bXFA = PDFUtils.isXFA(pdfBytes);
+				boolean bMCvi = isMCvi(file);
+				String sStdXML = null;
+				String sAdobeXML = null;
+				StdeCviXml stdXml = null;
+				if( bXFA || bMCvi ) {
+					if( bXFA ) {
+						Node xmlNode = PDFUtils.getXFADataNode(pdfBytes);
+						CoKsXML coks = new CoKsXML( xmlNode );
+						stdXml = coks.getStdeCviXml();
 						stdXml.setOriginalCVI( pdfBytes, file.getName() );
 						CviMetaDataXml metaData = new CviMetaDataXml();
 						metaData.setBureauReceiptDate( now );
 						stdXml.addMetadataAttachement( metaData );
-						sXML = stdXml.getXMLString();
+						if( bStd )
+							sStdXML = stdXml.getXMLString();
+						else
+							sAdobeXML = coks.toAcrobatXMLString();
+					}
+					else if( bMCvi ) {
+						String sDataFile = FileUtils.getRoot(file.getAbsolutePath()) + ".xml";
+						fDataFile =  new File(sDataFile);
+						String sV2XML = FileUtils.readTextFile(fDataFile);
+						sStdXML = V2Transform.convertToV1(sV2XML);
+						stdXml = new StdeCviXml( sStdXML );
+						stdXml.setOriginalCVI( pdfBytes, file.getName() );
+						CviMetaDataXml metaData = new CviMetaDataXml();
+						metaData.setBureauReceiptDate( now );
+						stdXml.addMetadataAttachement( metaData );
+						sStdXML = stdXml.getXMLString();
+					}
+					if( bStd ) {
 						if( bLoggedIn ) {
 							try {
 								CivetWebServices service = CivetWebServiceFactory.getService();
-								String sRet = service.sendCviXML(sXML);
+								String sRet = service.sendCviXML(sStdXML);
 								if( !sRet.contains(service.getSuccessMessage()) ) {
-									saveToXml(fXML, sXML);
+									saveToXml(fXMLOut, sStdXML);
 									logger.error("Could not submit to HERDS\nSaving to Robot XML folder");
-									logger.info("Processed File: " + file.getName() + " to: " + fXML.getName() );
+									logger.info("Processed File: " + file.getName() + " to: " + fXMLOut.getName() );
 									bLoggedIn = false;
 								}
 								logger.info("Processed File: " + file.getName() + " with CVI: " + stdXml.getCertificateNumber() +
 										" issued in " + stdXml.getOriginState() );
 							} catch( RemoteException re ) {
-								saveToXml(fXML, sXML);
+								saveToXml(fXMLOut, sStdXML);
 								logger.error("Could not submit to HERDS\nSaving to Robot XML folder", re);
-								logger.info("Processed File: " + file.getName() + " to: " + fXML.getName() );
+								logger.info("Processed File: " + file.getName() + " to: " + fXMLOut.getName() );
 								bLoggedIn = false;
 							}
 						}
 						else {
-							saveToXml(fXML, sXML);
+							saveToXml(fXMLOut, sStdXML);
 							logger.info("Processed File: " + file.getName() + " with CVI: " + stdXml.getCertificateNumber() +
 									" issued in " + stdXml.getOriginState() );
 						}
 					}
 					else {
-						sXML = coks.toAcrobatXMLString();
-						saveToXml(fXML, sXML);
-						logger.info("Processed File: " + file.getName() + " to: " + fXML.getName() );
+						saveToXml(fXMLOut, sAdobeXML);
+						logger.info("Processed File: " + file.getName() + " to: " + fXMLOut.getName() );
 					}
 				}
 				else {
-					logger.error( "File " + file.getName() + " was not a CO/KS XFA form");
+					logger.error( "File " + file.getName() + " was not a CO/KS XFA or mCVI form");
 				}
 				// Move Completed File to the compDir
 				File fPathOut = new File( compDir.getAbsolutePath() );
-				File fout = new File( fPathOut, file.getName() );
+				File fout = new File( fPathOut, fDataFile.getName() );
 				if( fout.exists() ) {
 					String sFOutPath = FileUtils.incrementFileName(fout.getAbsolutePath());
 					fout = new File( sFOutPath );
 				}
 				file.renameTo(fout);
+				if( bMCvi ) {
+					File fDataOut = new File( fPathOut, file.getName() );
+					if( fDataOut.exists() ) {
+						String sFOutPath = FileUtils.incrementFileName(fDataOut.getAbsolutePath());
+						fDataOut = new File( sFOutPath );
+					}
+					fDataFile.renameTo(fDataOut);
+				}
 			} catch (IOException e) {
-				logger.error("Error writing xml file " + fXML.getAbsolutePath(), e);
+				e.printStackTrace();
+				logger.error("Error writing xml file " + fXMLOut.getAbsolutePath(), e);
 			} catch (Exception e) {
+				e.printStackTrace();
 				logger.error("Error reading bytes from file " + file.getAbsolutePath(), e);
 			}
 		}
