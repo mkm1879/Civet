f8a2a18a86e4ca5fb5179a3d225d83505ed49adc
mkm1879@gmail.com <mkm1879@gmail.com>
2018-10-15 08:10:17 -0400

    Build 4.3 Beta

--- src/edu/clemson/lph/civet/CivetInbox.java
+++ src/edu/clemson/lph/civet/CivetInbox.java
@@ -63,7 +63,7 @@ import edu.clemson.lph.civet.vsps.VspsCviFile;
 
 @SuppressWarnings("serial")
 public class CivetInbox extends JFrame {
-	public static final String VERSION = "4.3d Alpha";
+	public static final String VERSION = "4.3 Beta Java 6";
 	private static final Logger logger = Logger.getLogger(Civet.class.getName());
 	static {
 	     logger.setLevel(CivetConfig.getLogLevel());
